e1a4ef693525b8df18531caf97a49590c9a7f258
abhikhatri67 <abhikhatri67@gmail.com>
2020-12-12 20:24:29 +0530

    Youtube clone in Vanilla JS.

---
+++ script.js
@@ -0,0 +1,10 @@
+// document.getElementById("myForm").addEventListener("onsubmit", (e) => {
+//   console.log("zdf");
+//   e.preventDefault();
+
+//   console.log("event: ", e);
+// });
+
+function myFunction(event) {
+  console.log("event: ", event);
+}
